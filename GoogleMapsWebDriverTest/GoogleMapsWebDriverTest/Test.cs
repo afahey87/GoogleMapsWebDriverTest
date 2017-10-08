06274657dfb2d6fe319dc71818cd5aa84c4136e7
Adam Fahey <afahey87@gmail.com>
2017-10-08 16:28:05 -0400

    Finished Simple Google Maps test
    Here is the finished simple google maps test.

--- GoogleMapsWebDriverTest/GoogleMapsWebDriverTest/Test.cs
+++ GoogleMapsWebDriverTest/GoogleMapsWebDriverTest/Test.cs
@@ -25,17 +25,21 @@ namespace Alerts_and_Windows
         [TearDown]
         public void teardown()
         {
-            //driver.Close();
-            //driver.Quit();
+            driver.Close();
+            driver.Quit();
         }
 
         [Test]
         public void GoogleMapsTest()
         {
-            Assert.True(driver.Url.Contains("maps"));
+            Assert.True(driver.Url.Contains("maps"));           //Verify we are on the expected webpage.
             driver.FindElement(By.XPath("//*[@id=\"searchboxinput\"]")).Click();
             driver.FindElement(By.XPath("//*[@id=\"searchboxinput\"]")).SendKeys("Portland");
             driver.FindElement(By.Id("searchbox-searchbutton")).Click();
+            driver.FindElement(By.XPath("//jsl/div[3]/div[8]/div[24]/div[1]/div[2]/div[8]/div/button[1]")).Click();
+            Assert.True(driver.Title.Contains("Google Maps"));          //Verify that the title has updated. This indicates that the search box has been cleared.
+            driver.FindElement(By.Id("sb_cb50")).Click();
+
         }
     }
 }
