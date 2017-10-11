4ea7811c2aaa83648379afded5233092cae40486
Fahey <adam.fahey@tylertech.com>
2017-10-10 21:06:05 -0400

    Added Comments and Clicked on a Photo
    Added Comments and Clicked on a Photo

--- GoogleMapsWebDriverTest/GoogleMapsWebDriverTest/Test.cs
+++ GoogleMapsWebDriverTest/GoogleMapsWebDriverTest/Test.cs
@@ -33,14 +33,27 @@ namespace Alerts_and_Windows
         [Test]
         public void GoogleMapsTest()
         {
-            Assert.True(driver.Url.Contains("maps"));           //Verify we are on the expected webpage.
+            // Verify we are on the expected webpage.
+            Assert.True(driver.Url.Contains("maps"));  
+            // Click into Search box.
             driver.FindElement(By.XPath("//*[@id=\"searchboxinput\"]")).Click();
+            // Send keys "Portlan"
             driver.FindElement(By.XPath("//*[@id=\"searchboxinput\"]")).SendKeys("Portland");
+            // Click the Search glass
             driver.FindElement(By.Id("searchbox-searchbutton")).Click();
+            System.Threading.Thread.Sleep(2000);
+            // Click Zoom in button. 
+            driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[4]/div[1]/button")).Click();
             System.Threading.Thread.Sleep(1000);
+            // Click photograph of Portland Customs House
+            driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[1]/div[1]/button[1]")).Click();
+            System.Threading.Thread.Sleep(2000);
+            // Click the back arrow
             driver.FindElement(By.XPath("//jsl/div[3]/div[8]/div[24]/div[1]/div[2]/div[8]/div/button[1]")).Click();
-            Assert.True(driver.Title.Contains("Google Maps"));  //Verify that the title has updated. This indicates that the search box has been cleared.
-            driver.FindElement(By.Id("sb_cb50")).Click();
+            // Click the X on the search box to clear it out. 
+            driver.FindElement(By.XPath("//*[@id=\"searchbox\"]/a")).Click();
+            //Verify that the title has updated. This indicates that the search box has been cleared.
+            Assert.True(driver.Title.Contains("Google Maps"));
 
         }
     }
